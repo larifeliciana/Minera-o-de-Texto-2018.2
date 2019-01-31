b15cc161f8690e4ac934f64c5a4b9505cbac2762
Larissa Feliciana <larissafelicianasilva@gmail.com>
2019-01-30 23:25:17 -0300

    lista 6

--- Lista 6/L6.py
+++ Lista 6/L6.py
@@ -1,9 +1,21 @@
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
-from L3Q5_Q6 import carregar
+#from lista3.L3Q5_Q6 import carregar
+import os
 from sklearn import feature_extraction
 from sklearn import metrics
 from sklearn import feature_selection
+
+
+def carregar(pasta):
+    caminhos = [os.path.join(pasta, nome) for nome in os.listdir(pasta)]
+    lista = []
+    for i in caminhos:
+        review = open(i, 'r')
+        lista.append(review.read())
+    return lista
+
+
 def cluster(idf,alg,k,selecionar):
     ##ler data
     d1 = carregar("business")
@@ -22,12 +34,13 @@ def cluster(idf,alg,k,selecionar):
 
 
     data = tipo.fit_transform(d1+d2+d3)
+
     if selecionar:
         selection = feature_selection.SelectKBest(feature_selection.chi2, k=k)
         data = selection.fit_transform(data, labels)
 
     if alg is "kmeans":
-        kmeans = KMeans(n_clusters=3,random_state=7).fit(data)
+        kmeans = KMeans(n_clusters=3,random_state=7,n_init=100, max_iter=1000).fit(data)
         pred = kmeans.labels_
 
 
@@ -37,17 +50,25 @@ def cluster(idf,alg,k,selecionar):
     #kmeans.labels_array(c1+c2+c3)
 
     print(metrics.accuracy_score(labels,pred))
-#    print(metrics.f1_score(labels, pred, average="weighted"))
+    print(metrics.f1_score(labels, pred, average="weighted"))
 print("xx")
 
-cluster(True, 'kmeans', 100,True)
+#tfidf
+#cluster(True, 'kmeans', 100,True)
+#counter
 cluster(False, 'kmeans', 100,True)
-cluster(True, 's', 100, True)
+#tfidf
+#cluster(True, 's', 100, True)
+#cluster
 cluster(False, 's', 100, True)
 
-
-cluster(True, 'kmeans', 100,False)
+print("\n\n\n")
+#tfidf
+#cluster(True, 'kmeans', 100,False)
+##counter
 cluster(False, 'kmeans', 100,False)
-cluster(True, 's', 100, False)
+#tfidf
+#cluster(True, 's', 100, False)
+#counter
 cluster(False, 's', 100, False   )
 
